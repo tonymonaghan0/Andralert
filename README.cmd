b687932a09e797cbc7f1eb165dff206d7814ea2a
tonymonaghan0 <49094693+tonymonaghan0@users.noreply.github.com>
2020-11-04 22:00:07 +0000

    Delete README.cmd

--- README.cmd
+++
@@ -1,3 +0,0 @@
-# Andralert
-Android intruder alarm that will ring or text another phone if an alert triggered.
-Will listen for an audio segnal that will trigger a call, text capture image or video
